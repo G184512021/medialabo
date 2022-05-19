733d5cd5a0c8bb1f049bc7b632eb25b621dcbc17
G184512021 <218451@st.takushoku-u.ac.jp>
2022-05-19 12:53:53 +0900

    a

---
+++ practice-dom/dom-demo.js
@@ -0,0 +1,14 @@
+let eu = document.querySelector('li#EU');
+eu.remove();
+let ul = document.querySelector('ul');
+let li;
+li = document.createElement('li');
+li.textContent = 'アジア大陸';
+ul.insertAdjacentElement('beforeend', li);
+li =document.createElement('li');
+li.textContent ='ヨーロッパ大陸';
+ul.insertAdjacentElement('beforeend',li);
+let h2s = document.querySelectorAll('h2');
+for (let h2 of h2s){
+    h2s.style.backgroundColor ='palegreen';
+  }
